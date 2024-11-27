0b8228811d9f8ef04a9923a65b25b84920df962f
KaiManian <karen.mnatsakanyan73@gmail.com>
2024-11-27 21:06:01 +0300

    Added Projects

---
+++ ExpensesTrackerLibrary/Contexts/ExpensesTrackerContext.cs
@@ -0,0 +1,30 @@
+﻿using ExpensesTrackerLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpensesTrackerLibrary.Contexts
+{
+    public class ExpensesTrackerContext : DbContext
+    {
+        //DbSet для категорий расходов
+        public DbSet<Category> Categories { get; set; }
+        //DbSet для самих расходов
+        public DbSet<Expense> Expenses { get; set; }
+
+        //Конструктор класса
+        public ExpensesTrackerContext()
+        {
+            //Удаляем существующую базу данных, если она есть
+            //Database.EnsureDeleted();
+
+            //Создаем новую базу данных, если ее нет
+            Database.EnsureCreated();
+        }
+
+        //Метод для настройки подключения к базе данных
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Используем Sqlite базу данных с указанием имени файла
+            optionsBuilder.UseSqlite("Data Source=Expenses.db");
+        }
+    }
+}
---
+++ ExpensesTrackerLibrary/Repositories/ExpenseRepository.cs
@@ -0,0 +1,83 @@
+﻿using ExpensesTrackerLibrary.Contexts;
+using ExpensesTrackerLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpensesTrackerLibrary.Repositories
+{
+    public class ExpenseRepository
+    {
+        private readonly ExpensesTrackerContext _context;
+        public ExpenseRepository(ExpensesTrackerContext context)
+        {
+            _context = context;
+        }
+
+        public void AddCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+        }
+
+        public void DeleteCategory(int categoryId)
+        {
+            var category = _context.Categories.Find(categoryId);
+            if(category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Category> GetAllCategories()
+        {
+            return _context.Categories.ToList();
+        }
+
+        public void AddExpense(Expense expense)
+        {
+            _context.Expenses.Add(expense);
+            _context.SaveChanges();
+        }
+
+        public void DeleteExpense(int expenseId)
+        {
+            var expense = _context.Expenses.Find(expenseId);
+            if (expense != null)
+            {
+                _context.Expenses.Remove(expense);
+                _context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Expense> GetAllExpense()
+        {
+            return _context.Expenses.ToList();
+        }
+
+        public IEnumerable<Expense> GetExpensesByCategoryName(string categoryName)
+        {
+            return _context.Expenses
+                .Where(e => e.Category.Name
+                .Contains(categoryName))
+                .ToList();
+        }
+
+        public IEnumerable<Expense> GetExpensesByCategory(int categoryId)
+        {
+            return _context.Expenses
+                .Include(e => e.Category)
+                .Where(e => e.CategoryId == categoryId)
+                .ToList();
+        }
+
+        public IEnumerable<Expense> GetExpensesByMonthYear(int month, int year)
+        {
+            return _context.Expenses
+                .Include(e => e.Category)
+                .Where(e => e.Date.Month == month && e.Date.Year == year)
+                .ToList();
+        }
+
+
+    }
+}
